84c35cc548edade872478a364a23c1b94bc36886
Deborah Venuti <dvenuti@wc-dhcp255d056.student-secure.wireless.fsu.edu>
2016-10-15 15:30:20 -0400

    Add User model

--- site/main/forms.py
+++ site/main/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    full_name = forms.CharField(label='Full Name', max_length=100)
+    first_name = forms.CharField(label='First Name', max_length=100)
+    last_name = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField(label='Email', max_length=150)
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', max_length=32, widget=forms.PasswordInput)
 
 class SignInForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=150)
--- site/main/models.py
+++ site/main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    full_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=150)
     password = models.CharField(max_length=32)
