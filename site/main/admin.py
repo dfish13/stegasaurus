1926fc0d54d58959cba95b6b674e8e224cadd150
Deborah Venuti <dvenuti@wc-dhcp255d056.student-secure.wireless.fsu.edu>
2016-10-15 17:57:15 -0400

    added authors to files

--- site/main/admin.py
+++ site/main/admin.py
@@ -1,3 +1,11 @@
+"""
+ This file was created on October 15th, 2016
+ by Deborah Venuti
+
+ Last updated on: October 15th, 2016
+ Updated by: Deborah Venuti
+"""
+
 from django.contrib import admin
 
 from .models import User
