dc173ce372e5646db5e7bb84e4b6666ba477dea4
Deborah Venuti <dvenuti@wc-dhcp255d056.student-secure.wireless.fsu.edu>
2016-10-15 15:08:35 -0400

    Adjust Forms

--- site/main/forms.py
+++ site/main/forms.py
@@ -3,9 +3,8 @@ from django import forms
 class RegisterForm(forms.Form):
     full_name = forms.CharField(label='Full Name', max_length=100)
     email = forms.EmailField(label='Email', max_length=150)
-    user_name = forms.CharField(label='Username', max_length=50)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
 class SignInForm(forms.Form):
-    user_name = forms.CharField(label='Username', max_length=50)
+    email = forms.EmailField(label='Email', max_length=150)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
--- site/main/models.py
+++ site/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=32)
