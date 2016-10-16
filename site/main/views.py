3fa61b477feb85cf34461af9b32437f44a776bdc
Deborah Venuti <dvenuti@timmy.local>
2016-10-16 16:55:37 -0400

    update file updated info

--- site/main/views.py
+++ site/main/views.py
@@ -2,7 +2,7 @@
  This file was created on October 15th, 2016
  by Deborah Venuti, Bethany Sanders and James Riley
 
- Last updated on: October 15th, 2016
+ Last updated on: October 16th, 2016
  Updated by: Deborah Venuti
 """
 
