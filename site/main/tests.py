61966c4dfe4f2d1387d26e53cdcd7fb0751cb138
Jennifer Yarboro <jyarboro@Jennifers-MacBook-Pro.local>
2016-10-18 23:58:46 -0400

    first attempt at running testing suite in django

--- site/main/tests.py
+++ site/main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestCalls(TestCase):
+	#def test_call_view_denies_anonymous_admin(self):
+	#	response = self.client.get('/signin', follow=True)
+	#	self.assertRedirects(response, 'main/signin.html')
+	#	response = self.client.post('/signin', follow=True)
+	#	self.assertRedirects(response, 'main/signin.html')
+
+	def test_call_view_loads(self):
+		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		response = self.client.get('/signin')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/signin.html')
+
+	def test_call_view_fails_blank(self):
+		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		response = self.client.post()
