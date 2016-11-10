4eee4cd064f1956bdb909b5ee8afd50671552cf8
Jennifer Yarboro <jyarboro@Jennifers-MacBook-Pro.local>
2016-11-09 19:15:31 -0500

    added tests for valid and invalid encrypt view calls, fixed blank login message test, added tests for valid and invalid register view calls

--- site/main/tests.py
+++ site/main/tests.py
@@ -2,7 +2,7 @@
  This file was created on October 18th, 2016
  by Jennifer Yarboro
 
- Last updated on: October 19th, 2016
+ Last updated on: November 9th, 2016
  Updated by: Jennifer Yarboro
 """
 
@@ -12,8 +12,12 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from . import views
 
-class TestCalls(TestCase):
 
+#this class mostly tests views
+#should be able to seperately test the views and other
+#page functionality in later iterations
+
+class TestCalls(TestCase):
 
 	#test index page
 	def test_index_view_loads(self):
@@ -48,35 +52,67 @@ class TestCalls(TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'main/profile.html')
 
+
+	#test encrypt redirect access
+	def test_encrypt_view_invalid(self):
+		self.user = User.objects.create_user(username='test@steg.com', password='testpassword')
+		self.user.save()
+
+		self.client.login(username='fake@steg.com', userpassword='fake')
+		response = self.client.get('/encrypt', follow=False)
+		self.assertEqual(response.status_code, 302)
+
+	#test encrypt access
+	def test_encrypt_view_valid(self):
+		self.user = User.objects.create_user(username='test@steg.com', password='testpassword')
+		self.user.save()
+
+		self.client.login(username='test@steg.com', password='testpassword')
+		response = self.client.get('/encrypt', follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/encrypt.html')
+
+
+
 	#test signin page
 	def test_signin_view_loads(self):
-		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		self.client.login(username='test@steg.com', userpassword='testpassword')
 		response = self.client.get('/signin')
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'main/signin.html')
 
 	#test valid login
 	def test_signin_view_valid(self):
-		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		self.client.login(username='test@steg.com', userpassword='testpassword')
 		response = self.client.post('/signin', {'username':'test@steg.com', 'password':'testpassword'}, follow=True)
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'main/signin.html')
 
 	#test invalid login
 	def test_signin_view_invalid(self):
-		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		self.client.login(username='test@steg.com', userpassword='testpassword')
 		response = self.client.post('/signin', {'username':'test@steg.com', 'password':'badpass'}, follow=False)
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'main/signin.html')
 
 	#test blank login message
-	#def test_call_view_fails_blank(self):
-	#	self.client.login(userName='test@steg.com', userPassword='testpassword')
-	#	response = self.client.post('/signin', {})
-	#	self.assertFormError(response, 'form', 'fieldname', 'expected error message')
+	def test_call_view_fails_blank(self):
+		self.client.login(userName='test@steg.com', userPassword='testpassword')
+		response = self.client.post('/signin', {})
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/signin.html')
 
-	#test invalid login message
-	#def test_call_view_fails_blank(self):
-	#	self.client.login(userName='test@steg.com', userPassword='testpassword')
-	#	response = self.client.post('/signin', {'invaliduser':'invalidpass'})
-	#	self.assertFormError(response, 'form', 'fieldname', 'expected error message')
+	#test register view
+	def test_call_register_view(self):
+		response = self.client.post('/register', {'first_name':'new', 'last_name':'user', 'email':'newuser@steg.com', 'password':'newpassword'})
+		self.assertEqual(response.status_code, 302)
+		self.assertRedirects(response, '/signin')
+
+	#test blank register
+	def test_blank_register_view(self):
+		response = self.client.post('/register', {'first_name':'', 'last_name':'', 'email':'', 'password':''})
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/register.html')
+
+
+#-------------------------END OF TEST SUITE----------------------
